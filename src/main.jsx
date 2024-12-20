aed39874c2b0db6a93a246411f1d5d4d06e19cc5
Jacques Toquard <jacques.toquard@proton.me>
2024-12-20 19:25:52 +0100

    feat(layout): snap footer to bottom

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import Router from "./router";
+import "./index.sass";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
