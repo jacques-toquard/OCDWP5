b89fcbb19b5f84a6819e33750d902eaf675ecfbc
Jacques Toquard <jacques.toquard@proton.me>
2024-12-20 20:11:24 +0100

    feat(pages): add not found page

---
+++ src/pages/NotFound/index.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router'
+import styles from './NotFound.module.sass'
+
+const NotFound = () => {
+  return (
+    <div className={styles.notFound}>
+      <h1>404</h1>
+      <p>Oups! La page que vous demandez n&apos;existe pas.</p>
+      <Link to="/">Retourner sur la page d&apos;accueil</Link>
+    </div>
+  )
+}
+
+export default NotFound
