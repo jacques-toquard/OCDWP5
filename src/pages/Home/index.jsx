66d53c1aa25afd21b5b835f825303feb43dfea29
Jacques Toquard <jacques.toquard@proton.me>
2024-12-24 12:50:50 +0100

    feat(home): remove Falaise component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,21 +1,25 @@
-import styles from './Home.module.sass'
-import logements from '../../assets/logements.json'
-import Falaise from '../../components/Falaise'
+import styles from "./Home.module.sass";
+import logements from "../../assets/logements.json";
+import GrayedImage from "../../components/GrayedImage";
+import falaise from "../../assets/falaise.png";
 
 const Home = () => {
-  return (
-    <div className={styles.home}>
-      <Falaise />
-      <div className={styles.grid}>
-        {logements.map(logement => (
-          <article key={logement.id} className={styles.card}>
-            <img src={logement.cover} alt={logement.title} />
-            <h2>{logement.title}</h2>
-          </article>
-        ))}
-      </div>
-    </div>
-  )
-}
+    return (
+        <div className={styles.home}>
+            <div className={styles.falaise}>
+                <GrayedImage src={falaise} alt="Falaise" />
+                <h1>Chez vous, partout et ailleurs</h1>
+            </div>
+            <div className={styles.grid}>
+                {logements.map((logement) => (
+                    <article key={logement.id} className={styles.card}>
+                        <img src={logement.cover} alt={logement.title} />
+                        <h2>{logement.title}</h2>
+                    </article>
+                ))}
+            </div>
+        </div>
+    );
+};
 
-export default Home
+export default Home;
