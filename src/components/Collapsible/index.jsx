b1f04dd25995f97e74bd2f768c4b79d8b246b642
Jacques Toquard <jacques.toquard@proton.me>
2024-12-25 22:48:00 +0100

    feat(about): add collapsibles

---
+++ src/components/Collapsible/index.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+import styles from "./Collapsible.module.sass";
+
+const Collapsible = ({ title, children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className={styles.collapsible}>
+      <button
+        className={`${styles.header} ${isOpen ? styles.open : ""}`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <h2>{title}</h2>
+        <span className={styles.arrow}>&#x2303;</span>
+      </button>
+      {isOpen && <div className={styles.content}>{children}</div>}
+    </div>
+  );
+};
+
+Collapsible.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+export default Collapsible;
--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,12 +1,37 @@
 import styles from "./About.module.sass";
 import GrayedImage from "../../components/GrayedImage";
 import paysage from "../../assets/paysage.png";
+import Collapsible from "../../components/Collapsible";
 
 const About = () => {
   return (
     <div className={styles.about}>
       <GrayedImage src={paysage} alt="Paysage" />
-      <h1>A propos</h1>
+      <div className={styles.collapsibles}>
+        <Collapsible title="Fiabilité">
+          Les annonces postées sur Kasa garantissent une fiabilité totale, Les
+          photos sont conformes aux logements, et toutes les informations sont
+          régulièrement vérifiées par nos équipes.
+        </Collapsible>
+        <Collapsible title="Respect">
+          La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+          comportement discriminatoire ou de perturbation du voisinage
+          entrainera une exclusion de notre plateforme.
+        </Collapsible>
+        <Collapsible title="Service">
+          La qualité du service est au cœur de notre engagement chez Kasa. Nous
+          veillons à ce que chaque interaction, que ce soit avec nos hôtes ou
+          nos locataires, soit empreinte de respect et de bienveillance.
+        </Collapsible>
+        <Collapsible title="Sécurité">
+          La sécurité est la priorité de Kasa. Aussi bïen pour nos hôtes que
+          pour les voyageurs, chaque logement correspond aux critères de
+          sécurité établis par nos services. En laissant une note aussi bien à
+          l&apos;hôte qu&apos;au locataire, cela permet à nos équipes de vérfier
+          que les standards sont bien respectés. Nous organisons également des
+          ateliers sur la sécurité domestique pour nos hôtes.
+        </Collapsible>
+      </div>
     </div>
   );
 };
